b8058d0e1eb00b9cc782520ca66360d473a6da41
AiFahri <fahrinurilll@gmail.com>
2025-10-31 10:02:39 +0700

    fix: Improve formatting and add missing ActivityDetail and AchievementEntry routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,12 @@
-import {BrowserRouter, Route, Routes} from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router";
 import AppLayout from "@/layout/AppLayout.tsx";
 import Home from "@/pages/Home";
-import {Activity} from "@/pages/Activity";
-import {Program} from "@/pages/Program";
-import {Achievement} from "@/pages/Achievement";
+import { Activity } from "@/pages/Activity";
+import ActivityDetail from "@/pages/Activity/Detail";
+import { Program } from "@/pages/Program";
+import ProgramDetail from "@/pages/Program/Detail";
+import { Achievement } from "@/pages/Achievement";
+import AchievementEntry from "@/pages/Achievement/Entry";
 import About from "@/pages/About";
 import Madrasah from "@/pages/Education/Madrasah";
 import TK from "@/pages/Education/TK";
@@ -18,28 +21,31 @@ import Sanggar from "@/pages/Education/Sanggar";
 import LKSA from "@/pages/Education/LKSA";
 
 export default function AppRoutes() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route element={<AppLayout/>}>
-                    <Route path="/" element={<Home/>}/>
-                    <Route path="/kegiatan" element={<Activity/>}/>
-                    <Route path="/program" element={<Program/>}/>
-                    <Route path="/prestasi" element={<Achievement/>}/>
-                    <Route path="/tentang" element={<About/>}/>
-                    <Route path="/pendidikan/madrasah" element={<Madrasah/>}/>
-                    <Route path="/pendidikan/tk" element={<TK/>}/>
-                    <Route path="/pendidikan/smp" element={<SMP/>}/>
-                    <Route path="/pendidikan/smk" element={<SMK/>}/>
-                    <Route path="/pendidikan/tahfidz" element={<Tahfidz/>}/>
-                    <Route path="/pendidikan/pesantren" element={<Pesantren/>}/>
-                    <Route path="/pendidikan/diniyah" element={<Diniyah/>}/>
-                    <Route path="/pendidikan/tpq" element={<TPQ/>}/>
-                    <Route path="/pendidikan/bq" element={<BQ/>}/>
-                    <Route path="/pendidikan/sanggar" element={<Sanggar/>}/>
-                    <Route path="/pendidikan/lksa" element={<LKSA/>}/>
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<AppLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/kegiatan" element={<Activity />} />
+          <Route path="/kegiatan/:slug" element={<ActivityDetail />} />
+          <Route path="/program" element={<Program />} />
+          <Route path="/program/:slug" element={<ProgramDetail />} />
+          <Route path="/prestasi" element={<Achievement />} />
+          <Route path="/prestasi/:slug" element={<AchievementEntry />} />
+          <Route path="/tentang" element={<About />} />
+          <Route path="/pendidikan/madrasah" element={<Madrasah />} />
+          <Route path="/pendidikan/tk" element={<TK />} />
+          <Route path="/pendidikan/smp" element={<SMP />} />
+          <Route path="/pendidikan/smk" element={<SMK />} />
+          <Route path="/pendidikan/tahfidz" element={<Tahfidz />} />
+          <Route path="/pendidikan/pesantren" element={<Pesantren />} />
+          <Route path="/pendidikan/diniyah" element={<Diniyah />} />
+          <Route path="/pendidikan/tpq" element={<TPQ />} />
+          <Route path="/pendidikan/bq" element={<BQ />} />
+          <Route path="/pendidikan/sanggar" element={<Sanggar />} />
+          <Route path="/pendidikan/lksa" element={<LKSA />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
